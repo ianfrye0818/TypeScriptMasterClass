e0fc73260b81f3d8758f9bb43d1e6f21c21e6759
Ian Frye <ian_frye@outlook.com>
2023-12-24 17:21:30 -0500

    adding lesson 5

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log('this is watchin');
+//------------------
+// arrays
+//------------------
+let names: string[] = ['Mario', 'Lugigi', 'Peach'];
+let ages: number[] = [34, 23, 25];
+
+//can only push types declared above
+names.push('Toad');
+ages.push(34);
+
+//type inferrence w/ arrays
+let fruits = ['apples', 'pears', 'bananas', 'mangos'];
+fruits.push('mango');
+
+const f = fruits[3];
+
+//inferrs a union type
+let things = [1, true, 'hello'];
+
+//does not know exactly what type things is -> can be one of the types above
+const t = things[0];
+
+//------------------
+//object literals
+//--------------------
+
+let user: {} = {
+  name: 'Ian',
+  age: 37,
+  id: 1,
+};
+
+//type inference with object literals
