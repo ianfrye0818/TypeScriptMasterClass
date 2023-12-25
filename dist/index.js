12586f4d68fce447c79f33eef412d58abb4f7900
Ian Frye <ian_frye@outlook.com>
2023-12-25 06:26:15 -0500

    adding type-guards branch

--- dist/index.js
+++ dist/index.js
@@ -16,5 +16,27 @@ function swapIdType(id) {
     // can only use methods that are common between all types
     //not valid as typescript does not know this will for sure be a string
     // parseInt(id)
-    return id;
+    // could check type of before running
+    if (typeof id === 'string') {
+        // can do anything specific to strings
+        return parseInt(id);
+    }
+    else {
+        //can use anything specific to numbers
+        return id.toString();
+    }
+}
+const idOne = swapIdType(1);
+console.log('id one: ', typeof idOne); //id one: string
+const idTwo = swapIdType('2');
+console.log('id two: ', typeof idTwo); //id two: number
+function logDetails(value) {
+    if (value.type === 'user') {
+        // typescript knows the type passed in is a User type
+        console.log(value.email, value.username);
+    }
+    if (value.type === 'person') {
+        //typescript knows the type passed in is the Person type
+        console.log(value.firstName, value.age);
+    }
 }
