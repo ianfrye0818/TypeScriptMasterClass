14dca77dcd48e3346df9c341f2c1d84eb6eb4eeb
Ian Frye <ian_frye@outlook.com>
2024-01-06 09:19:31 -0500

    adding comments to generics

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,13 @@ interface Collection<T> {
   name: string;
 }
 
+//Collection with T = string
 const collection1: Collection<string> = {
   data: ['mario', 'yoshi', 'peach', 'toad'],
   name: 'Nintendo Characters',
 };
 
+//Collection with T = number
 const collection2: Collection<number> = {
   data: [20, 15, 256, 3456],
   name: 'Winning Lottery Numbers',
@@ -20,40 +22,52 @@ function randomCollectionItem<T>(c: Collection<T>): T {
   return c.data[i];
 }
 
+//implicitly set the type of the collection with the <string>
 const character = randomCollectionItem<string>(collection1);
+
+//infers the type based on the collection that was passed it to it
 const lottery = randomCollectionItem(collection2);
 
 console.log(character, lottery);
 
 //Generic Classes &&
 //generic constraints
-
-interface HasID {
-  id: number;
-}
-
 class DataCollection<T extends HasID> {
   constructor(private data: T[]) {}
 
+  //load one item at random
   loadOne() {
     const i = Math.floor(Math.random() * this.data.length);
     return this.data[i];
   }
+
+  //get all items
   loadAll() {
     return this.data;
   }
+
+  //add an item and return
   add(value: T): T[] {
     this.data.push(value);
     return this.data;
   }
 
-  delete(id: number): void {
+  //delete an item
+  delete(id: number): T[] {
     this.data = this.data.filter((item) => item.id !== id);
+    return this.data;
   }
 }
+
+//defines a User objects
 interface User {
   name: string;
   score: number;
+  id: number;
+}
+
+//extends the collection type allowing for use of the ID (Used the delete method)
+interface HasID {
   id: number;
 }
 
@@ -64,8 +78,13 @@ const users = new DataCollection<User>([
 ]);
 
 users.add({ name: 'Jordan', score: 234, id: 4 });
+
 console.log(users.loadAll());
+
 const user = users.loadOne();
+
 users.delete(2);
+
 const allUsers = users.loadAll();
+
 console.log(allUsers);
